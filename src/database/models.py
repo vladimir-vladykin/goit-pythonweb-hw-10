e29a8836ec35782f6683ff2e8417037f2c150e12
Volodymyr Vladykin <vova@pocketguard.com>
2025-10-12 19:16:55 +0300

    Add User ORM model; Perform Alembic migration

--- src/database/models.py
+++ src/database/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime, date
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
-from sqlalchemy import Integer, String, func
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, relationship
+from sqlalchemy import Integer, String, func, Column, ForeignKey
 from sqlalchemy.sql.sqltypes import DateTime, Date
 
 
@@ -20,3 +20,17 @@ class Contact(Base):
     created_at: Mapped[datetime] = mapped_column(
         "created_at", DateTime, default=func.now()
     )
+    user_id = Column(
+        "user_id", ForeignKey("users.id", ondelete="CASCADE"), default=None
+    )
+    user = relationship("User", backref="notes")
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
+    email = Column(String, unique=True)
+    hashed_password = Column(String)
+    created_at = Column(DateTime, default=func.now())
+    avatar = Column(String(255), nullable=True)
