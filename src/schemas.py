ab8872665b70c09625ca69a81c0a32b0e6e02d25
Volodymyr Vladykin <vova@pocketguard.com>
2025-10-12 19:32:59 +0300

    Add new schemas for User

--- src/schemas.py
+++ src/schemas.py
@@ -16,3 +16,23 @@ class ContactModel(BaseModel):
 class ContactModelResponse(ContactModel):
     id: int
     created_at: datetime
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+    avatar: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
