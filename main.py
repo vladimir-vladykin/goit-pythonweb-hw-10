0c7536d880ebdc960def27254d805d5cd5f745d0
Volodymyr Vladykin <vova@pocketguard.com>
2025-10-13 21:10:03 +0300

    Add CORS

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.api import contacts, auth, users
+
+origins = ["<http://localhost:3000>"]
 
 app = FastAPI()
 app.include_router(contacts.router, prefix="/api")
 app.include_router(auth.router, prefix="/api")
 app.include_router(users.router, prefix="/api")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def run():
