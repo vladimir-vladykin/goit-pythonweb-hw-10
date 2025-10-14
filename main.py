903fbe230772b7898bfbd0ed583c4c32e97441ff
Volodymyr Vladykin <vova@pocketguard.com>
2025-10-14 10:56:34 +0300

    Add limits for /me request

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from src.api import contacts, auth, users
+from slowapi import Limiter
+from slowapi.errors import RateLimitExceeded
+from slowapi.util import get_remote_address
+from starlette.responses import JSONResponse
 
 origins = ["<http://localhost:3000>"]
+limiter = Limiter(key_func=get_remote_address)
 
 app = FastAPI()
+
+
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
+    return JSONResponse(
+        status_code=429,
+        content={"error": "Requests limit exceeded, try later"},
+    )
+
+
 app.include_router(contacts.router, prefix="/api")
 app.include_router(auth.router, prefix="/api")
 app.include_router(users.router, prefix="/api")
