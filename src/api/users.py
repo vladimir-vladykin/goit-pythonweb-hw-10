cafa7be3cb74ac26ae4b615eac8f3fd055baf9f3
Volodymyr Vladykin <vova@pocketguard.com>
2025-10-15 20:53:49 +0300

    Implement User avatar update

--- src/api/users.py
+++ src/api/users.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, File, UploadFile
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.services.upload_file import UploadFileService
+from src.services.users import UserService
+from src.database.db import get_db
+from src.conf.config import settings
 from src.schemas import User
 from src.services.auth import get_current_user
 from slowapi import Limiter
@@ -13,4 +18,22 @@ limiter = Limiter(key_func=get_remote_address)
 )
 @limiter.limit("5/minute")
 async def me(request: Request, user: User = Depends(get_current_user)):
+    return user
+
+
+@router.patch("/avatar", response_model=User)
+async def update_avatar_user(
+    file: UploadFile = File(),
+    user: User = Depends(get_current_user),
+    db: AsyncSession = Depends(get_db),
+):
+    avatar_url = UploadFileService(
+        settings.CLOUDINARY_NAME,
+        settings.CLOUDINARY_API_KEY,
+        settings.CLOUDINARY_API_SECRET,
+    ).upload_file(file, user.username)
+
+    user_service = UserService(db)
+    user = await user_service.update_avatar_url(user.email, avatar_url)
+
     return user
--- src/conf/config.py
+++ src/conf/config.py
@@ -3,7 +3,6 @@ from pydantic import ConfigDict
 from pydantic_settings import BaseSettings
 
 
-
 class Settings(BaseSettings):
     DB_URL: str
     JWT_SECRET: str
@@ -13,6 +12,10 @@ class Settings(BaseSettings):
     MAIL_USERNAME: str
     MAIL_PASSWORD: str
     MAIL_FROM: str
+
+    CLOUDINARY_NAME: str
+    CLOUDINARY_API_KEY: int
+    CLOUDINARY_API_SECRET: str
 
     model_config = ConfigDict(
         extra="ignore", env_file=".env", env_file_encoding="utf-8", case_sensitive=True
